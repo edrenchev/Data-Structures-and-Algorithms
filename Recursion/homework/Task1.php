06429cb28c93f7c098d22ab7c39c69f55a5d0c0c
Ervin <edrenchev@gmail.com>
2016-11-28 09:49:59 +0200

    Recursion: Add Task 1

---
+++ Recursion/homework/Task1.php
@@ -0,0 +1,31 @@
+<?php
+
+class Task1 {
+
+	public static $taken = [];
+
+	public static $n = 3;
+
+	public static function Main() {
+		self::variate(0);
+	}
+
+	public static function printNum($i) {
+		for ($l = 0; $l < $i; $l++) print((self::$taken[$l] + 1). ' ');
+		print ("\r\n");
+	}
+
+	public static function variate($i) {
+
+		if($i >= self::$n) {self::printNum($i); return; }
+		for ($j = 0; $j < self::$n; $j++) {
+			self::$taken[$i] = $j;
+			self::variate($i + 1);
+		}
+
+	}
+
+
+}
+
+Task1::Main();
